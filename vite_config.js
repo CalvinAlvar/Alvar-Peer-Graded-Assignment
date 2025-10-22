ea1f66364c2319d9df64eae109caac7b0f5027f2
Calvin Alvar <422001008@ntc.edu.ph>
2025-10-22 19:17:30 +0800

    first commit

--- vite_config.js
+++ vite_config.js
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Alvar-Peer-Graded-Assignment/",   // Add this line for GitHub Pages deployment
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
 });
